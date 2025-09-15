a1e6b8364f9a5cdd10c724f8a19c442f05948942
vmeggiolaro <vmeggiolaro@udesa.edu.ar>
2025-09-15 09:38:16 -0300

    rutas hechas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Switch>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/peliculas" exact={true} ></Route>
+        <Route path="/series" exact={true} ></Route>
+        <Route path="/detalle" exact={true} ></Route>
+        <Route path="/favoritos" exact={true} ></Route>
+        <Route path="/resultados-busqueda" exact={true} ></Route>
+        <Route path="" component={Error}/>
+      </Switch>
   );
 }
 
