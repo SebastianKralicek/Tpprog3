8a507bcba43a3aa00cad87264c0b4b01adf72ba1
SebastianKralicek <skralicek@udesa.edu.ar>
2025-09-15 09:38:08 -0300

    organizado

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from '../../components/Navbar/Navbar'
+
+function Home(){
+    return(
+        <React.Fragment>
+            <Navbar name="Header"/>
+
+
+            <Footer />
+        </React.Fragment>
+    )
+}
+
+export default Home;
